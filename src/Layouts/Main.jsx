b1f3590b39972e19a1cc46dd4b5c112821ea59f0
MD Rahi <fhrstation21@gmail.com>
2024-01-05 09:34:56 +0600

    Project Initialization Completed. Also Make Layout and Initial Home, Header and Footer components

---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Components/Shared/Footer";
+import Header from "../Components/Shared/Header";
+
+const Main = () => {
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    );
+};
+
+export default Main;
