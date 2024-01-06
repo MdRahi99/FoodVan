13bb1120729217c3c5788a552059cd6c298e7f7c
MD Rahi <fhrstation21@gmail.com>
2024-01-06 11:32:59 +0600

    create context api to access fetched data from anywhere in the application

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,30 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useContext } from 'react';
 import Banner from "./Banner";
 import Popular from "./Popular";
 import Recommended from "./Recommended";
-import axios from 'axios';
 import Loader from '../Shared/Loader';
+import { DataContext } from '../../Contexts/DataProvider';
 
 const Home = () => {
 
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('http://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10');
-                setData(response.data.Items);
-            } catch (error) {
-                setError(error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchData();
-    }, []);
+    const {data, loading, error} = useContext(DataContext);
 
     const filterPopularData = data?.filter(item => {
         return item.IsPopular === true
---
+++ src/Contexts/DataProvider.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types */
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+
+export const DataContext = createContext();
+
+const DataProvider = ({ children }) => {
+
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://www.api.technicaltest.quadtheoryltd.com/api/Item?page=1&pageSize=10');
+                setData(response.data.Items);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const value = { data, loading, error };
+
+    return <DataContext.Provider value={value}>
+        {children}
+    </DataContext.Provider>;
+};
+
+export default DataProvider;
