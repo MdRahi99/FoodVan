55cead4fce8670ae4b13dfec624263d88e46046d
MD Rahi <fhrstation21@gmail.com>
2024-01-07 15:14:17 +0600

    add shadow in item card items

--- src/Components/ItemCard/ItemCard.jsx
+++ src/Components/ItemCard/ItemCard.jsx
@@ -4,8 +4,8 @@ const ItemCard = (items) => {
 
     return (
         <>
-            <div>
-                <img className="h-44 w-full lg:h-64 lg:w-64 rounded-2xl" src={ImageUrl ? ImageUrl :'Loading...'} alt={Name} />
+            <div className="w-full">
+                <img className="h-40 w-full lg:h-64 lg:w-64 rounded-2xl shadow-2xl shadow-stone-400" src={ImageUrl ? ImageUrl :'Loading...'} alt={Name} />
                 <h1 className="text-center text-slate-600 mt-1">{Name}</h1>
             </div>
         </>
